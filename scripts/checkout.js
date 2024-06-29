15e84131074899b8c12327860b0e550f8ef2f84f
ayush3031 <ayushraj4435@gmail.com>
2024-06-29 09:56:37 +0530

    end

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,11 @@ import {renderOrderSummary,getItemQuantity} from './checkout/orderSummary.js'
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 //import '../data/cart-class.js';
-
-
+console.log(p);
+const p = new Promise(()=>{
+    console.log('promise');
+})
+//Promise();
 //import '../data/backend-practice.js'
 //loadProducts(renderOrderSummary);
 //loadProducts(renderPaymentSummary);
